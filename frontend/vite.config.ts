3e536b55c59e4ea8b8da864b17b99cd0bb1a2a74
treysisson <trey.sisson@gmail.com>
2025-06-19 18:38:51 -0700

    fix(build): implement definitive postcss configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
 })
